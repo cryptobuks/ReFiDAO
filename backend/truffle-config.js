ac31a3664beb5b482ddffd6d6274585f6f9dace7
e-swissolar <vasileios.panagiotidis@gmail>
2022-08-10 21:16:59 +0200

    create folder for artifacts

--- backend/truffle-config.js
+++ backend/truffle-config.js
@@ -35,7 +35,7 @@ module.exports = {
    * $ truffle test --network <network-name>
    */
 
-  contracts_build_directory: "../backend/contracts",
+  contracts_build_directory: "../backend/contracts/artifacts",
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
